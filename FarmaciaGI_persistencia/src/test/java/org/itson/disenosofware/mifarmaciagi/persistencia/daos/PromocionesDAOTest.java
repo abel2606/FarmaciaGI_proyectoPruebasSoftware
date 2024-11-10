423be05ea47c12ad06830fb4b5d9bed7eaf4ff6c
imnotrichi <ricardo.gutierrez247398@potros.itson.edu.mx>
2024-11-09 21:33:15 -0700

    PromocionesDAOTest

--- FarmaciaGI_persistencia/src/test/java/org/itson/disenosofware/mifarmaciagi/persistencia/daos/PromocionesDAOTest.java
+++ FarmaciaGI_persistencia/src/test/java/org/itson/disenosofware/mifarmaciagi/persistencia/daos/PromocionesDAOTest.java
@@ -3,6 +3,7 @@
  */
 package org.itson.disenosofware.mifarmaciagi.persistencia.daos;
 
+import java.util.LinkedList;
 import java.util.List;
 import org.itson.disenosofware.mifarmaciagi.persistencia.Exception.PersistenciaException;
 import org.itson.disenosofware.mifarmaciagi.persistencia.entidades.Producto;
@@ -23,10 +24,7 @@ public class PromocionesDAOTest {
 
     @Mock
     private IPromocionesDAO promocionesDAO;
-
-    /**
-     * Test of obtenerPromocion method, of class PromocionesDAO.
-     */
+    
     @Test
     public void ObtenerPromocion_PromocionExistente_ReturnSuccess() {
         //arrange
@@ -42,20 +40,6 @@ public class PromocionesDAOTest {
     }
 
     @Test
-    public void ObtenerPromocion_PromocionExistente_ReturnFailure() {
-        //arrange
-        Promocion promocionBuscada = new Promocion("PRO-001", "Paracetamol 2x1", new Producto("Paracetamol", 20.0f, "PAR-001"), 2, 10.0f);
-        when(promocionesDAO.obtenerPromocion(new Promocion("PRO-001"))).thenReturn(new Promocion("PRO-201", "Simi Paletas -$50.00", new Producto("Simi Paletas", 120.0f, "SIM-001"), 1, 70.0f));
-        Promocion resultado;
-
-        //act
-        resultado = promocionesDAO.obtenerPromocion(new Promocion("PRO-001"));
-
-        //assert
-        assertNotEquals(promocionBuscada, resultado);
-    }
-
-    @Test
     public void ObtenerPromocion_PromocionInexistente_ReturnNull() {
         //arrange
         when(promocionesDAO.obtenerPromocion(new Promocion("PRO-002"))).thenReturn(null);
@@ -68,11 +52,8 @@ public class PromocionesDAOTest {
         assertNull(resultado);
     }
 
-    /**
-     * Test of registrarPromocion method, of class PromocionesDAO.
-     */
     @Test
-    public void RegistrarPromocion_PromocionNueva_ReturnSuccess() throws Exception {
+    public void RegistrarPromocion_PromocionNueva_PromocionRegistrada() throws Exception {
         //arrange
         Promocion promocionNueva = new Promocion("PRO-005", "Omeprazol 2X1", new Producto("Omeprazol", 35.0f, "OME-011"), 2, 17.5f);
         doNothing().when(promocionesDAO).registrarPromocion(promocionNueva);
@@ -102,11 +83,8 @@ public class PromocionesDAOTest {
         assertNotNull(e);
     }
 
-    /**
-     * Test of actualizarPromocion method, of class PromocionesDAO.
-     */
     @Test
-    public void ActualizarPromocion_PromocionExistente_ReturnSuccess() throws Exception {
+    public void ActualizarPromocion_PromocionExistente_PromocionActualizada() throws Exception {
         //arrange
         Promocion promocionActualida = new Promocion("PRO-005", "Omeprazol 3X2", new Producto("Omeprazol", 35.0f, "OME-011"), 3, 23.3333f);
         doNothing().when(promocionesDAO).actualizarPromocion(promocionActualida);
@@ -136,11 +114,8 @@ public class PromocionesDAOTest {
         assertNotNull(e);
     }
 
-    /**
-     * Test of eliminarPromocion method, of class PromocionesDAO.
-     */
     @Test
-    public void EliminarPromocion_PromocionExistente_ReturnSuccess() throws Exception {
+    public void EliminarPromocion_PromocionExistente_PromocionEliminada() throws Exception {
         //arrange
         Promocion promocionEliminar = new Promocion("PRO-001");
         doNothing().when(promocionesDAO).eliminarPromocion(promocionEliminar);
@@ -174,9 +149,24 @@ public class PromocionesDAOTest {
      * Test of obtenerPromociones method, of class PromocionesDAO.
      */
     @Test
-    public void ObtenerPromociones_ListaPromociones_ReturnSuccess() {
+    public void ObtenerPromociones_ListaPromociones_ListaLlena() {
         //arrange
         List<Promocion> promociones = List.of(new Promocion("PRO-002"), new Promocion("PRO-002"), new Promocion("PRO-003"));
+        when(promocionesDAO.obtenerPromociones()).thenReturn(promociones);
+        List<Promocion> resultado;
+
+        //act
+        resultado = promocionesDAO.obtenerPromociones();
+
+        //assert
+        assertEquals(promociones, resultado);
+        assertEquals(promociones.size(), resultado.size());
+    }
+    
+    @Test
+    public void ObtenerPromociones_ListaPromociones_ListaVacía() {
+        //arrange
+        List<Promocion> promociones = new LinkedList<>();
         when(promocionesDAO.obtenerPromociones()).thenReturn(promociones);
         List<Promocion> resultado;
 
